fd5fae5b421340cfb57ac5084f29b9eb0e014653
AyrielNoriega <anoriega2310@gmail.com>
2022-08-07 13:00:57 -0500

    tipos de componentes class, function, const

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//componente con una funcion
 // function Hello (props){
 //   const {title} = props
 //   return <h1>{title}</h1>
 // }
 
-const Hello = (props) => <h2>{props.title}</h2>
+//componente con una constante
+//const Hello = (props) => <h2>{props.title}</h2>
+
+//componente con una clase
+class Hello extends Component {
+  render() { //el metodo render no tiene parametros, se hacer referncia a las props con this
+    return <h2>{this.props.title}</h2>
+  }
+}
 
 function App() {
   return (
