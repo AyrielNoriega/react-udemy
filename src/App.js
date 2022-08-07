85b28d5cf40048ecce131ce055e87c82fbbcda95
AyrielNoriega <anoriega2310@gmail.com>
2022-08-07 12:39:23 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function Hello (props){
+//   const {title} = props
+//   return <h1>{title}</h1>
+// }
+
+const Hello = (props) => <h2>{props.title}</h2>
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello title="Hello from props"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
